af76d0909b148ff9deff6b83d06007523e92b030
Lasse R <lasserafn@gmail.com>
2023-06-13 19:04:20 +0800

    Create ProjectBuilder.php

---
+++ src/Builders/ProjectBuilder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Rackbeat\Client\Builders;
+
+use Rackbeat\Models\Project;
+
+class ProjectBuilder extends Builder
+{
+	protected $entity = 'projects';
+	protected $model = Project::class;
+
+	public function upsert($data): Project
+	{
+		// todo
+	}
+}
