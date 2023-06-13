2cccff0a120cf3a3ac37d02d4a2dfdb0bd3a5522
Lasse R <lasserafn@gmail.com>
2023-06-13 19:16:36 +0800

    Update ProjectBuilder.php

--- src/Builders/ProjectBuilder.php
+++ src/Builders/ProjectBuilder.php
@@ -11,6 +11,13 @@ class ProjectBuilder extends Builder
 
 	public function upsert($data): Project
 	{
-		// todo
+		return $this->request->handleWithExceptions(function () use ($data) {
+		    $response =$this->request->client->post("{$this->entity}/upsert", [
+			'json' => $data
+		    ]);
+		    $responseData = $this->getResponse($response);
+
+		    return $this->populateModelsFromResponse($responseData->first());
+		});
 	}
 }
