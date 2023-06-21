b6e054223830e01c940be1046325fffaacd2a225
lasserafn <lasserafn@gmail.com>
2023-06-21 20:40:52 +0800

    Fix project

--- src/Builders/ProjectBuilder.php
+++ src/Builders/ProjectBuilder.php
@@ -12,9 +12,8 @@ class ProjectBuilder extends Builder
 	public function upsert($data): Project
 	{
 		return $this->request->handleWithExceptions(function () use ($data) {
-			$response =$this->request->client->post("{$this->entity}/upsert", [
-				'json' => $data
-			]);
+			$response =$this->request->getClient()->asJson()->post("{$this->entity}/upsert", $data);
+
 			$responseData = $this->getResponse($response);
 
 			return $this->populateModelsFromResponse($responseData->first());
